4f40b4eaf09443a4a8aa4e9b0bc0279386620d04
Tom-OriginStorage <jefferson.chu@chainup.com>
2022-10-13 14:29:42 +0800

    Update upgrade script

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -1,8 +1,8 @@
 const { ethers, upgrades, run } = require("hardhat");
 
 // TO DO: Place the address of your proxy here!
-const proxyValidatorController = "0x7Cbc3e3F12Aae70175479721c8E2a226d16D0De7";
-const proxyAggregator = "0xf7c34C051C65fd57168B9083F7414b5045A87276";
+const proxyVault = "0xD78736E9EE9895A0a35AB19163Bd1A94E72dea3e";
+const proxyAggregator = "0x789983D968074df2395608700C561D4AD959f189";
 
 async function upgrade(contract: String, proxyAddress: String) {
   const Contract = await ethers.getContractFactory(contract);
@@ -14,8 +14,23 @@ async function upgrade(contract: String, proxyAddress: String) {
   });
 }
 
+async function upgradev2(contract: String, proxyAddress: String) {
+  const Contract = await ethers.getContractFactory(contract);
+  console.log("Preparing upgrade...");
+  const c = await upgrades.prepareUpgrade(proxyAddress, Contract);
+  console.log("Upgrade at:", c);
+
+  await proxyAddress.upgradeTo(c);
+  await run("verify:verify", {
+    address: c,
+    constructorArguments: [],
+  });
+}
+
 async function main() {
-  await upgrade("Aggregator", proxyAggregator);
+  // await upgrade("NodeRewardVault", proxyVault);
+  // await upgradev2("NodeRewardVault", proxyVault);
+  await upgradev2("Aggregator", proxyAggregator);
   //await upgrade("ValidatorController", proxyValidatorController);
 }
 
