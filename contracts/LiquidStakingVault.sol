8ee302ffceebfc82760dae940a4b2d62578d8f61
derrick [OriginStorage] <108317502+derricktan23@users.noreply.github.com>
2022-11-09 13:50:20 +0800

    Liquid Staking Vault - Deposit Function and KETH.sol (LP)

--- contracts/LiquidStakingVault.sol
+++ contracts/LiquidStakingVault.sol
@@ -2,23 +2,26 @@
 
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "./KEth.sol";
 
-contract LiquidStakingVault is UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable, ERC20Upgradeable {
-    uint256 private constant MINIMUM = 100_000_000_000;
-    address private _aggregatorProxyAddress;
-    uint256 public underlyingEth;
+contract LiquidStakingVault is UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable, KEth {
+// contract LiquidStakingVault is KEth {
 
-    event AggregatorChanged(address _from, address _to);
+    // uint256 private constant MINIMUM = 100_000_000_000;
+    // address private _aggregatorProxyAddress;
+    // uint256 public underlyingEth;
 
-    modifier onlyAggregator() {
-        require(_aggregatorProxyAddress == msg.sender, "Not allowed to touch funds");
-        _;
-    }
+    event DepositReceived(address indexed from, uint256 amount, uint256 time);
+
+    // event AggregatorChanged(address _from, address _to);
+
+    // modifier onlyAggregator() {
+    //     require(_aggregatorProxyAddress == msg.sender, "Not allowed to touch funds");
+    //     _;
+    // }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {}
@@ -27,43 +30,62 @@ contract LiquidStakingVault is UUPSUpgradeable, OwnableUpgradeable, ReentrancyGu
         __Ownable_init();
         __UUPSUpgradeable_init();
         __ReentrancyGuard_init();
-        __ERC20_init("kETH", "King Ether");
-
-        _aggregatorProxyAddress = address(0x1);
+        // _aggregatorProxyAddress = address(0x1);
+        _mint(_msgSender(), 10000000000000000000000);
     }
+
 
     function _authorizeUpgrade(address) internal override onlyOwner {}
 
-    function _shareAmount(uint256 amount) private view returns (uint256) {
-        return (totalSupply() * amount) / underlyingEth;
+
+
+     function deposit() external payable  {
+        // Check deposit settings
+        require(msg.value >= 0.00000000001 ether, "The deposited amount is less than the minimum deposit size");
+
+        //  Calculate deposit fee
+        uint256 depositFee = msg.value * 0.00001 ether / 1 ether ;
+        uint256 depositNet = msg.value - depositFee ;
+
+        //  Mint rETH to user account
+        _mintKETH(msg.sender, depositNet);
+        // rocketTokenRETH.mint(depositNet, msg.sender);
+
+        // Process deposit
+        //  Emit deposit received event
+        emit DepositReceived(msg.sender, msg.value, block.timestamp);
     }
 
-    function _etherAmount(uint256 amount) private view returns (uint256) {
-        return (underlyingEth * amount) / totalSupply();
-    }
 
-    function mint(address account, uint256 amount) external payable onlyAggregator nonReentrant {
-        require(msg.value == amount, "Ether amount provided is not sufficient");
-        require(msg.value > MINIMUM, "Ether provided is less than minimum amount");
-        uint256 shares = _shareAmount(amount);
-        _mint(account, shares);
+    // function _shareAmount(uint256 amount) private view returns (uint256) {
+    //     return (totalSupply() * amount) / underlyingEth;
+    // }
 
-        underlyingEth += amount;
-    }
+    // function _etherAmount(uint256 amount) private view returns (uint256) {
+    //     return (underlyingEth * amount) / totalSupply();
+    // }
 
-    function redeem(address account, uint256 amount) external onlyAggregator nonReentrant {
-        uint256 ethers = _etherAmount(amount);
+    // function mint(address account, uint256 amount) external payable onlyAggregator nonReentrant {
+    //     require(msg.value == amount, "Ether amount provided is not sufficient");
+    //     require(msg.value > MINIMUM, "Ether provided is less than minimum amount");
+    //     uint256 shares = _shareAmount(amount);
+    //     _mint(account, shares);
+
+    //     underlyingEth += amount;
+    // }
+
+    // function redeem(address account, uint256 amount) external onlyAggregator nonReentrant {
+    //     uint256 ethers = _etherAmount(amount);
         
-        payable(account).transfer(ethers);
-        _burn(account, amount);
+    //     payable(account).transfer(ethers);
+    //     _burn(account, amount);
 
-        underlyingEth -= ethers;
-    }
-
-    function setAggregator(address aggregatorProxyAddress_) external onlyOwner {
-        require(aggregatorProxyAddress_ != address(0), "Aggregator address provided invalid");
-        emit AggregatorChanged(_aggregatorProxyAddress, aggregatorProxyAddress_);
-        _aggregatorProxyAddress = aggregatorProxyAddress_;
-    }
+    //     underlyingEth -= ethers;
+    // }
+    // function setAggregator(address aggregatorProxyAddress_) external onlyOwner {
+    //     require(aggregatorProxyAddress_ != address(0), "Aggregator address provided invalid");
+    //     emit AggregatorChanged(_aggregatorProxyAddress, aggregatorProxyAddress_);
+    //     _aggregatorProxyAddress = aggregatorProxyAddress_;
+    // }
 
 }
