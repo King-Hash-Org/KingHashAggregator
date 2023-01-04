ebc421743b65ad598a5203595dd48c8353562931
chainupk <kenneth.goh@chainup.com>
2023-01-04 14:11:30 +0800

    test space

--- contracts/Aggregator.sol
+++ contracts/Aggregator.sol
@@ -9,6 +9,7 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "./interfaces/IAggregator.sol";
 import "./routes/ValidatorNftRouter.sol";
 
+
 /**
  * @title Staking Aggregator for Ethereum Network
  * @notice Accepts incoming data and route the Ether to different strategies
